1287a908951292065b8600bb0ee26a418c71d20d
Bald Charm <972839577@qq.com>
2022-12-23 13:03:21 +0900

    Create settings page.

---
+++ src/main/java/com/baldcharm/blog/controller/admin/AdminSettingsController.java
@@ -0,0 +1,14 @@
+package com.baldcharm.blog.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminSettingsController {
+    @GetMapping("/settings")
+    public String getSettingsPage() {
+        return "admin/settings";
+    }
+}
